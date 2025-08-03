b12afe7ca5504e97f9789779645762ba5a12fd7d
jgolden5 <jonathan.golden.gurr@gmail.com>
2025-08-03 09:36:02 -0700

    state amount inflation comparer function

--- state-wage-comparisons.sh
+++ state-wage-comparisons.sh
@@ -1,155 +1,158 @@
 #!/bin/bash
-get_state_amount() {
+get_state_house_inflation() {
   case $1 in
-    arkansas)
-      echo 1.17
-      ;;
-    mississippi)
-      echo 1.16
-      ;;
     alabama)
-      echo 1.16
-      ;;
-    west_virginia)
-      echo 1.14
-      ;;
-    kentucky)
-      echo 1.14
-      ;;
-    south_dakota)
-      echo 1.14
-      ;;
-    oklahoma)
-      echo 1.13
-      ;;
-    ohio)
-      echo 1.13
-      ;;
-    missouri)
-      echo 1.13
-      ;;
-    louisiana)
-      echo 1.12
-      ;;
-    iowa)
-      echo 1.12
-      ;;
-    indiana)
-      echo 1.12
-      ;;
-    nebraska)
-      echo 1.12
-      ;;
-    kansas)
-      echo 1.11
-      ;;
-    north_dakota)
-      echo 1.10
-      ;;
-    south_carolina)
-      echo 1.10
-      ;;
-    new_mexico)
-      echo 1.10
-      ;;
-    north_carolina)
-      echo 1.09
-      ;;
-    wisconsin)
-      echo 1.09
-      ;;
-    michigan)
-      echo 1.08
-      ;;
-    idaho)
-      echo 1.08
-      ;;
-    wyoming)
-      echo 1.08
-      ;;
-    georgia)
-      echo 1.08
-      ;;
-    montana)
-      echo 1.07
-      ;;
-    arizona)
-      echo 1.04
-      ;;
-    utah)
-      echo 1.04
-      ;;
-    texas)
-      echo 1.03
-      ;;
-    nevada)
-      echo 1.03
-      ;;
-    minnesota)
-      echo 1.03
-      ;;
-    pennsylvania)
-      echo 1.03
-      ;;
-    illinois)
-      echo 1.02
-      ;;
-    delaware)
-      echo 1.01
-      ;;
-    rhode_island)
-      echo 1.01
-      ;;
-    maine)
-      echo 1.00
-      ;;
-    florida)
-      echo 0.99
-      ;;
-    oregon)
-      echo 0.99
-      ;;
-    colorado)
-      echo 0.98
-      ;;
-    virginia)
-      echo 0.98
-      ;;
-    vermont)
-      echo 0.97
+      echo 0.6370
       ;;
     alaska)
-      echo 0.95
+      echo 1.0051
       ;;
-    new_hampshire)
-      echo 0.94
+    arizona)
+      echo 1.2271
       ;;
-    connecticut)
-      echo 0.94
-      ;;
-    washington)
-      echo 0.93
-      ;;
-    maryland)
-      echo 0.92
-      ;;
-    massachusetts)
-      echo 0.91
-      ;;
-    new_jersey)
-      echo 0.87
+    arkansas)
+      echo 0.5718
       ;;
     california)
-      echo 0.87
+      echo 2.2006
+      ;;
+    colorado)
+      echo 1.5505
+      ;;
+    connecticut)
+      echo 1.1051
+      ;;
+    delaware)
+      echo 1.0763
       ;;
     dc)
-      echo 0.86
+      echo 1.7559
       ;;
-    new_york)
-      echo 0.86
+    florida)
+      echo 1.1282
+      ;;
+    georgia)
+      echo 0.9255
       ;;
     hawaii)
-      echo 0.85
+      echo 2.4129
+      ;;
+    idaho)
+      echo 1.2755
+      ;;
+    illinois)
+      echo 0.7226
+      ;;
+    indiana)
+      echo 0.6659
+      ;;
+    iowa)
+      echo 0.6004
+      ;;
+    kansas)
+      echo 0.6250
+      ;;
+    kentucky)
+      echo 0.5653
+      ;;
+    louisiana)
+      echo 0.5588
+      ;;
+    maine)
+      echo 1.1003
+      ;;
+    maryland)
+      echo 1.1672
+      ;;
+    massachusetts)
+      echo 1.7152
+      ;;
+    michigan)
+      echo 0.6687
+      ;;
+    minnesota)
+      echo 0.9290
+      ;;
+    mississippi)
+      echo 0.4935
+      ;;
+    missouri)
+      echo 0.6848
+      ;;
+    montana)
+      echo 1.2891
+      ;;
+    nebraska)
+      echo 0.7228
+      ;;
+    nevada)
+      echo 1.2259
+      ;;
+    new_hampshire)
+      echo 1.3084
+      ;;
+    new_jersey)
+      echo 1.4478
+      ;;
+    new_mexico)
+      echo 0.8406
+      ;;
+    new_york)
+      echo 1.3032
+      ;;
+    north_carolina)
+      echo 0.9276
+      ;;
+    north_dakota)
+      echo 0.7133
+      ;;
+    ohio)
+      echo 0.6261
+      ;;
+    oklahoma)
+      echo 0.5734
+      ;;
+    oregon)
+      echo 1.4013
+      ;;
+    pennsylvania)
+      echo 0.7350
+      ;;
+    rhode_island)
+      echo 1.2617
+      ;;
+    south_carolina)
+      echo 0.8279
+      ;;
+    south_dakota)
+      echo 0.8414
+      ;;
+    tennessee)
+      echo 0.8959
+      ;;
+    texas)
+      echo 0.8588
+      ;;
+    utah)
+      echo 1.4651
+      ;;
+    vermont)
+      echo 1.0727
+      ;;
+    virgina)
+      echo 1.0862
+      ;;
+    washington)
+      echo 1.6562
+      ;;
+    west_virginia)
+      echo 0.4472
+      ;;
+    wisconsin)
+      echo 0.8236
+      ;;
+    wyoming)
+      echo 0.9628
       ;;
     *)
       echo "State not recognized"
@@ -157,11 +160,28 @@ get_state_amount() {
   esac
 }
 
+house_price_conversion() { #$1 = state 1; $2 state 2; $3 = dollar amount in state 1; output = dollar conversion of $3 from state 1 ($1) to state 2 ($2)
+  local state_1="$(get_state_house_inflation $1)"
+  local state_2="$(get_state_house_inflation $2)"
+  local house_1_price="$3"
+  echo "$1 = $state_1"
+  echo "$2 = $state_2"
+  echo "Average House Price in $1 = \$$house_1_price"
+  #for example, house_1_price_conversion california mississippi 10000000
+  local house_2_price="$(echo "scale=2; $house_1_price * $state_2 / $state_1" | bc)"
+  if [[ $house_2_price == .* ]]; then
+    house_2_price="\$0$house_2_price"
+  else
+    house_2_price="\$$house_2_price"
+  fi
+  echo "Price in $2 = $house_2_price"
+}
+
 price_of_good_in_state() {
   #note that each of the base prices of the following items are based on their average price in the state of Maine, which is the exact middle of states when it comes to inflated vs deflated usd
   good="$1"
   state="$2"
-  state_amount=$(get_state_amount $state)
+  state_amount=$(get_state_house_inflation $state)
   case "$good" in
     egg|eggs|dozen_eggs)
       name_of_good="A dozen eggs"
